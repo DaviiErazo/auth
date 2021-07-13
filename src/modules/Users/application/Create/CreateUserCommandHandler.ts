6500c2185e6dd16edde768e9326c843791367978
David Erazo <39785169+DaviiErazo@users.noreply.github.com>
2021-07-13 16:31:12 -0400

    Update CreateUserCommandHandler.ts

--- src/modules/Users/application/Create/CreateUserCommandHandler.ts
+++ src/modules/Users/application/Create/CreateUserCommandHandler.ts
@@ -16,6 +16,7 @@ export class CreateUserCommandHandler implements CommandHandler<CreateUserComman
     const username = UserName.create({ value: command.username });
     const password = UserPassword.create({ value: command.password });
     const email = UserEmail.create({ value: command.email });
+    
     await this.userCreator.run({ username, password, email });
   }
 }
