5ddfa3cbc75837816da9e91463881620530dfa11
David <da.erazom@gmail.com>
2021-07-13 16:38:58 -0400

    change logs on redisConnection

--- src/modules/Users/infrastructure/services/Redis/RedisConnection.ts
+++ src/modules/Users/infrastructure/services/Redis/RedisConnection.ts
@@ -1,10 +1,11 @@
 import redis, { RedisClient } from "redis";
 import IRedisConfig from "../../../../Shared/infrastructure/Services/redis/IRedisConfig";
+import Logger from "../../../../../modules/Shared/domain/Logger";
 
 const isProduction = process.env.NODE_ENV === "prod";
 
 export class RedisConnection {
-  static createClient(redisConfig: IRedisConfig): RedisClient {
+  static createClient(redisConfig: IRedisConfig, logger: Logger): RedisClient {
     const port = redisConfig.redisServerPort;
     const host = redisConfig.redisServerURL;
     const redisConnection: RedisClient = isProduction
@@ -12,7 +13,7 @@ export class RedisConnection {
       : redis.createClient(port, host); // creates a new client
 
     redisConnection.on("connect", () => {
-      console.log(`[Redis]: Connected to redis server at ${host}:${port}`);
+      logger.info(`[Redis]: Connected to redis server at ${host}:${port}`);
     });
 
     return redisConnection;
