bb98f8224d5d285c322cc76026f4e2e03138a17e
David <da.erazom@gmail.com>
2021-07-13 10:18:43 -0400

    remove recipient

--- src/modules/Recipients/application/RecipientsResponse.ts
+++
@@ -1,9 +0,0 @@
-import { Recipient } from "../domain/Recipient";
-
-export class RecipientsResponse {
-  readonly recipients: Array<Recipient>;
-
-  constructor(recipients: Array<Recipient>) {
-    this.recipients = recipients;
-  }
-}
--- src/modules/Recipients/domain/RecipientRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Criteria } from "../../Shared/domain/criteria/Criteria";
-import { Recipient } from "./Recipient";
-
-export interface RecipientRepository {
-  save(recipient: Recipient): Promise<void>;
-  matching(criteria: Criteria): Promise<Array<Recipient>>;
-}
